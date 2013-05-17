08c72622ee313e75affbb594c8b4e7cc9f2f4bb0
Blaza <radebeka@gmail.com>
2013-05-17 13:20:52 +0200

    Fix and update models. Add admin autoregister

--- pitanja/models.py
+++ pitanja/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
-class Question(models.Model):
-    number = models.IntegeField()
-    text = models.TextField()
-    image = models.ImageField()
-    answer_type = models.ForeignKey(AnswerType)
-
-
 class AnswerType(models.Model):
     name = models.CharField(max_length=15)
+
+    def __unicode__(self):
+    	return self.name
+
+
+class Question(models.Model):
+    number = models.IntegerField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    answer_type = models.ForeignKey(AnswerType)
+
+    def __unicode__(self):
+    	return self.number
 
 
 class Answer(models.Model):
     text = models.TextField()
     value = models.CharField(max_length=150)
     answer_type = models.ForeignKey(AnswerType)
+
+    def __unicode__(self):
+    	return self.text
